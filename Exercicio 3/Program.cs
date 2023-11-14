0e0faa5afe042997dc214383f0f997891192646d
Henrique dalla cort <henriquedallacort@hotmail.com>
2023-11-14 14:55:32 -0300

    Ex 3 - V1.0

--- Exercicio 3/Program.cs
+++ Exercicio 3/Program.cs
@@ -1,19 +1,34 @@
-﻿// using SkiaSharp;
+﻿using SkiaSharp;
 
-// namespace Projeto {
-// 	class Program {
-// 		static void Main(string[] args) {
-// 			// ****************************************************************************
-// 			// Esse código é só um começo! :)
-// 			// ****************************************************************************
+namespace Projeto {
+	class Program {
+		static void Main(string[] args) {
 
-// 			using (SKBitmap bitmap = SKBitmap.Decode("Caminho da imagem de entrada")) {
-// 				unsafe {
+			using (SKBitmap bitmapEntrada = SKBitmap.Decode("C:\\Users\\HENRIQUE.CORT\\Desktop\\ComputaçãoCognitiva\\Atv2\\Atv_Individual2\\Exercicio 3\\entrada\\Exercicio 3.png")){
+				
+				unsafe {
+					byte* entrada = (byte*)bitmapEntrada.GetPixels();		
 
-// 					byte* ptr = (byte*)bitmap.GetPixels();
+					int largura = bitmapEntrada.Width;
+					int altura = bitmapEntrada.Height;
+					int pixelsTotais = bitmapEntrada.Width * bitmapEntrada.Height;
+					int [] pixels= new int[256];
+                    int pixelsContados = 0;
 
-// 				}
-// 			}
-// 		}
-// 	}
-// }
+					for (int y = 0; y < altura ; y++) {
+						for (int x= 0; x < largura ; x++) {
+							pixels[entrada[y * largura + x ]] += 1;
+						}
+					}
+
+                    for (int z=0; z<=255; z++){
+                        Console.WriteLine("Quantidade de pixels com valor "+ z +": "+ pixels[z]);
+                        pixelsContados += pixels[z];
+                    }   
+                    Console.WriteLine("Pixels Totais: " + pixelsTotais);
+                    Console.WriteLine("Pixels contados: " + pixelsContados);
+                }
+			}
+		}
+	}
+}
